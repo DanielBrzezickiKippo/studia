5cc83d23939763345b9145a6463c5b71dac5ef57
DanielBrzezickiKippo <56343240+DanielBrzezickiKippo@users.noreply.github.com>
2019-11-13 11:44:30 +0100

    Update Losowanie_liczb.cpp

--- Losowanie_liczb.cpp
+++ Losowanie_liczb.cpp
@@ -44,4 +44,53 @@ int main()
 		Sleep(1000);
 	}
 }
+/*#include "pch.h"
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
 
+
+int main()
+{
+	int tab[10];
+	int line = 0;
+	int column = 0;
+	int counter = 0;
+	srand(time(NULL));
+
+
+	DWORD start = GetTickCount();
+	while (GetTickCount() - start<15000) {
+		system("cls");
+		for (int i = 0; i <= 9; i++) {
+			tab[i] = rand() % 10;
+			printf_s("%d", tab[i]);
+		}
+		printf_s("\n");
+
+		while (line <= 9) {
+			column = 0;
+			while (column <= 9) {
+				if (line < tab[column]) {
+					printf_s("#");
+				}
+				else {
+					printf_s(" ");
+				}
+
+				column++;
+			}
+
+			line++;
+			printf_s("\n");
+		}
+
+		line = 0;
+		column = 0;
+		if (round(GetTickCount()-start % 1000) != 0) {
+			Sleep(950);
+		}
+	}
+	printf("Czas wykonywania: %lu ms\n", GetTickCount() - start);
+}*/
